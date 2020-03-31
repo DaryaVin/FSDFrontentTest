258894db4d8e2512b50795f7bfaac3c193783ff5
DaryaVin <DaryaVin@gmail.com>
2020-03-31 12:29:13 +0700

    The dropdownField block is created. The textField block is changed . Classes for flex layout is added to the baseStyle block.

--- src/desktop.bundles/UIKit/UIKit.js
+++ src/desktop.bundles/UIKit/UIKit.js
@@ -1,4 +1,5 @@
 import './UIKit.scss';
-import '@common/baseStyle/baseStyle.scss';
 import '@common/logo/logo.scss';
 import '@common/textField/textField.scss';
+import '@common/dropdownField/dropdownField.scss';
+import '@common/baseStyle/baseStyle.scss';
