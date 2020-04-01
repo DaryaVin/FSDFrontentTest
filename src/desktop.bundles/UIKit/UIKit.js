e28bfa8f1abc53109b6ee2da8e8a736cd561b50b
DaryaVin <DaryaVin@gmail.com>
2020-04-01 14:28:53 +0700

    Button block is created.

--- src/desktop.bundles/UIKit/UIKit.js
+++ src/desktop.bundles/UIKit/UIKit.js
@@ -1,5 +1,7 @@
-import './UIKit.scss';
 import '@common/logo/logo.scss';
 import '@common/textField/textField.scss';
 import '@common/dropdownField/dropdownField.scss';
+import '@common/button/button.scss';
 import '@common/baseStyle/baseStyle.scss';
+
+import './UIKit.scss';
