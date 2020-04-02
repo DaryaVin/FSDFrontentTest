5c613146a5e75182d0f48c29f1bbab09890e9fa5
DaryaVin <DaryaVin@gmail.com>
2020-04-02 11:02:32 +0700

    radioButton block is created.

--- src/desktop.bundles/UIKit/UIKit.js
+++ src/desktop.bundles/UIKit/UIKit.js
@@ -3,5 +3,6 @@ import '@common/textField/textField.scss';
 import '@common/dropdownField/dropdownField.scss';
 import '@common/button/button.scss';
 import '@common/baseStyle/baseStyle.scss';
+import '@common/radioButton/radioButton.scss';
 
 import './UIKit.scss';
