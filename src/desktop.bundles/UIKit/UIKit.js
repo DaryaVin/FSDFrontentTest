59f82d49c82d6fa95903c9a2b828ce18acbc2e02
DaryaVin <DaryaVin@gmail.com>
2020-04-03 16:54:29 +0700

    likeButton block is created.

--- src/desktop.bundles/UIKit/UIKit.js
+++ src/desktop.bundles/UIKit/UIKit.js
@@ -2,6 +2,7 @@ import '@common/logo/logo.scss';
 import '@common/textField/textField.scss';
 import '@common/dropdownField/dropdownField.scss';
 import '@common/button/button.scss';
+import '@common/likeButton/likeButton.scss';
 import '@common/baseStyle/baseStyle.scss';
 import '@common/radioButton/radioButton.scss';
 import '@common/checkboxButton/checkboxButton.scss';
