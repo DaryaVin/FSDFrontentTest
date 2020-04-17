9ce37201fb8dec8e1d25535fb1750a4269af9a4d
DaryaVin <DaryaVin@gmail.com>
2020-04-17 09:32:38 +0700

    dropdown block is changed.

--- src/common.blocks/dropdown/dropdown.js
+++ src/common.blocks/dropdown/dropdown.js
@@ -1,19 +1,23 @@
 import '@common/dropdown/dropdown.scss';
-window.onclick = function(event) {
-  if (!event.target.matches('.dropdown *')) {
-    var dropdownConteners = $(".dropdown__dropdownContener");
-    var dropdownButtons = $(".dropdown__dropdownButton");
-    for (var i = 0; i < dropdownConteners.length; i++) {
-      var openDropdown = dropdownConteners[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
+
+$('.dropdown').each(function() {
+  var dropdown = $(this),
+  dropdownContener = dropdown.find('.dropdown__dropdownContener'),
+  dropdownButton = dropdown.find('.dropdown__dropdownButton');
+
+    dropdownButton.click(function() {
+      dropdownButton.toggleClass("formField__field_version_dropdown");
+      dropdownContener.toggleClass("show");
+    });
+
+    $(document).mouseup(function(event) {
+      if (!dropdown.is(event.target) && dropdown.has(event.target).length === 0) {
+        if (dropdownContener.hasClass('show')) {
+          dropdownContener.removeClass('show');
+        }
+        if (dropdownButton.hasClass('formField__field_version_dropdown')) {
+          dropdownButton.removeClass('formField__field_version_dropdown');
+        }
       }
-    }
-    for (var i = 0; i < dropdownButtons.length; i++) {
-      var openDropdown = dropdownButtons[i];
-      if (openDropdown.classList.contains('formField__field_version_dropdown')) {
-        openDropdown.classList.remove('formField__field_version_dropdown');
-      }
-    }
-  }
-}
+    });
+})
--- src/common.blocks/dropdown/dropdown__dropdownButton.js
+++
@@ -1,4 +0,0 @@
-function dropdownButton_OnClick(id) {
-  document.getElementById("dropdown__dropdownContener" + id).classList.toggle("show");
-  document.getElementById("dropdown__dropdownButton" + id).classList.toggle("formField__field_version_dropdown");
-};
