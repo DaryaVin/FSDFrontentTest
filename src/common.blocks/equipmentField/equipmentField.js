8b34dd1b0434b5dd0adc3491607c84a3dd580c50
DaryaVin <DaryaVin@gmail.com>
2020-04-17 12:04:41 +0700

    equipmentField block is created. dropdownField block is deleted.

---
+++ src/common.blocks/equipmentField/equipmentField.js
@@ -0,0 +1,62 @@
+import '@common/equipmentField/equipmentField.scss';
+
+$('.equipmentField').each(function() {
+  var equipmentField = $(this),
+    title = equipmentField.find('.dropdown__dropdownButton p'),
+    numbers = equipmentField.find('input[type="number"].numberPicker__number');
+    title.text("Удобства номера");
+
+    function numberVariantStr(n) {
+      var variantStr = -1;
+      switch (n % 10) {
+        case 0:
+          variantStr = 0;
+          break;
+        case 1:
+          if (n % 100 == 11) {
+            variantStr = 0;
+          } else {
+            variantStr = 1;
+          };
+          break;
+        case 2:
+        case 3:
+        case 4:
+          if ((10 < (n % 100)) && ((n % 100)< 20)) {
+            variantStr = 0;
+          } else {
+            variantStr = 2;
+          };
+          break;
+        case 5:
+        case 6:
+        case 7:
+        case 8:
+        case 9:
+          variantStr = 0;
+          break;
+        default:
+          variantStr = -1;
+      }
+      return variantStr;
+   }
+
+    numbers.change(function(){
+      var variantStr = [
+        ["спален", "спальня", "спальни"],
+        ["кроватей", "кровать", "кровати"],
+        ["ванных комнат", "ванная комната", "ванные комнаты"]
+      ];
+      var titleStr = '';
+      for (let i = 0; i < numbers.length; i++) {
+        var numberVal = parseFloat(numbers[i].value);
+        var variant =  numberVariantStr(numberVal);
+        if ((variant < 0) || (variant > 2)) variant = 2;
+        if (numberVal > 0) {
+          if (titleStr != '') titleStr += ", ";
+          titleStr += numberVal + " " + variantStr[i][variant];
+        }
+      };
+      title.text(titleStr);
+    });
+})
--- src/desktop.bundles/UIKit/UIKit.js
+++ src/desktop.bundles/UIKit/UIKit.js
@@ -1,6 +1,5 @@
 import '@common/logo/logo.scss';
 import '@common/textField/textField.scss';
-import '@common/dropdownField/dropdownField.js';
 import '@common/dropdown/dropdown.js';
 import '@common/button/button.scss';
 import '@common/likeButton/likeButton.scss';
@@ -16,5 +15,6 @@ import '@common/pagination/pagination.js';
 import '@common/numberPicker/numberPicker.js';
 import '@common/numberPickerList/numberPickerList.scss';
 import '@common/guestsField/guestsField.js';
+import '@common/equipmentField/equipmentField.js';
 
 import './UIKit.scss';
